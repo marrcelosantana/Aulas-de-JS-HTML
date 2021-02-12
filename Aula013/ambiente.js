2e58f8564d47ac09cc6ac104f5add111f0148429
Marcelo Santana <47793587+marrcelosantana@users.noreply.github.com>
2021-02-12 18:59:55 -0300

    Add files via upload

---
+++ Aula013/ambiente.js
@@ -0,0 +1,9 @@
+var cont = 1;
+
+do{
+    console.log(`Passo ${cont}`);
+    cont++;
+}
+while(cont <= 6){
+}
+
---
+++ Aula14/ambiente.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...');
+
+for(var cont = 1; cont <= 10; cont++){
+    console.log(`Passo ${cont}`);
+}
+
+console.log('FIM!');
